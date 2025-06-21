9194f0c69f5118900c727b4055df75c7ac29fd73
TNTStudios <tntstudiosn@gmail.com>
2025-06-21 02:48:38 -0600

    minijuego maze

--- src/main/java/com/TNTStudios/deWaltCore/DeWaltCore.java
+++ src/main/java/com/TNTStudios/deWaltCore/DeWaltCore.java
@@ -1,35 +1,37 @@
 package com.TNTStudios.deWaltCore;
 
+import com.TNTStudios.deWaltCore.minigames.MinigameListener;
+import com.TNTStudios.deWaltCore.minigames.maze.MazeCommand;
+import com.TNTStudios.deWaltCore.minigames.maze.MazeManager;
+import com.TNTStudios.deWaltCore.points.PointsManager;
 import com.TNTStudios.deWaltCore.registration.RegistrationListener;
 import com.TNTStudios.deWaltCore.scoreboard.ScoreboardListener;
-import com.TNTStudios.deWaltCore.stats.PointsManager; // Importo mi nuevo manager
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class DeWaltCore extends JavaPlugin {
 
-    // Creo una instancia estática para poder acceder al manager desde otras clases
-    private static PointsManager pointsManager;
-
     @Override
     public void onEnable() {
-        // Inicio el PointsManager para que cargue los datos antes que nada
-        pointsManager = new PointsManager(this);
-
-        // Registro los eventos del scoreboard
+        // --- Scoreboard y Registro (Existente) ---
         getServer().getPluginManager().registerEvents(new ScoreboardListener(), this);
+        // getServer().getPluginManager().registerEvents(new RegistrationListener(), this); // Sigue apagado
 
-        // register apagado
-        //getServer().getPluginManager().registerEvents(new RegistrationListener(), this);
+        // --- Sistema de Minijuego de Laberinto (Nuevo) ---
+        // 1. Inicializo mis nuevos managers
+        PointsManager pointsManager = new PointsManager(this);
+        MazeManager mazeManager = new MazeManager(this, pointsManager);
+
+        // 2. Registro los nuevos comandos
+        MazeCommand mazeCommand = new MazeCommand(mazeManager);
+        getCommand("empezar").setExecutor(mazeCommand);
+        getCommand("detener").setExecutor(mazeCommand);
+
+        // 3. Registro el nuevo listener para eventos de minijuegos
+        getServer().getPluginManager().registerEvents(new MinigameListener(mazeManager), this);
     }
 
     @Override
     public void onDisable() {
-        // Guardo todos los puntos cuando el servidor se apaga para que no se pierda nada
-        pointsManager.savePoints();
-    }
-
-    // Con este método estático, cualquier otra clase puede obtener el PointsManager
-    public static PointsManager getPointsManager() {
-        return pointsManager;
+        // Aquí podríamos guardar datos si fuera necesario, pero mi PointsManager guarda al instante.
     }
 }
